d545f6a34318d557a6f00f098df50accc5be8dd8
gdpashov <gdpashov@abv.bg>
2023-02-03 20:33:10 +0200

    Initial commit

---
+++ SwiftUICombineLibrary/SwiftUICombineLibrary/SwiftUICombineLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineLibraryApp.swift
+//  SwiftUICombineLibrary
+//
+//  Created by Georgi Pashov on 3.02.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
